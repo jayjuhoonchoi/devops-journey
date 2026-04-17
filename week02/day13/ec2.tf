0a75089b7ab4ab6a1009772f3914e65c8ae17422
jhchoi <a01046765331@gmail.com>
2026-04-17 17:36:47 +0900

    day13: ansible playbook - nginx, docker auto configuration

---
+++ week02/day13/ec2.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "devops-journey-day13-key"
+  public_key = file("~/.ssh/devops-journey-key.pub")
+}
+
+resource "aws_security_group" "web" {
+  name = "devops-journey-day13-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  tags = { Name = "devops-journey-day13" }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
